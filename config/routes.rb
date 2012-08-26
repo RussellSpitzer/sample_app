9de4579bbe0ca917edb2acd81abd20a675a6f22d
Russell <russell.spitzer@gmail.com>
2012-08-26 11:04:15 -0700

    Finish Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SampleApp::Application.routes.draw do 
 
+  resources :sessions, only:[:new,:create,:destroy]
   resources :users
 
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy' , via: :delete
   match '/home',	to: 'static_pages#home'
   match '/contact',	to: 'static_pages#contact'
   match '/help',	to: 'static_pages#help'
